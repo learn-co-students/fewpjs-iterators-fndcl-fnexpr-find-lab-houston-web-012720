88344e36a8cbfa763ceed17f46d4a49efbea1a14
timothyalton <mitchell.alton@live.com>
2020-03-18 15:57:07 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  record.find(record => record.result === "W")
+  if (record.result === "W"){
+    return record.year
+  }
+}
+
+// this should work... im done with this remote crap 
+

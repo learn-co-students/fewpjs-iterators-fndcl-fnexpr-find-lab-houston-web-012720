a5de532183e981d614055f41b84cf2d78afc7bcd
anamsoomro <anamsoomroed@gmail.com>
2020-03-18 15:07:44 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let win = record.find((game) => game.result === "W")
+  return win ? win.year : win
+}
+
+const a = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]

c4b039289e3da4fb2e6e55aa62a0bfe7369cff93
SteRobWms <stephen.r.williams.87@gmail.com>
2020-03-19 09:44:13 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arr) => {
+  const win = arr.find(element => element.result === "W")
+  if (win != undefined){
+    return win.year
+  }
+  else {return win}
+  
+}
